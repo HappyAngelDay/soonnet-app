11d0aa19ed49e8fb3d8041f9696119162be51bf1
Alice Lai Man Sze <alice_lai@SOONEST_HK>
2025-06-19 12:49:21 +0800

    fix: set base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/soonnet-app/', // ⬅️⬅️⬅️ 重點設定！
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
