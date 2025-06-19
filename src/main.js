fb0c186ae369e807ff66c890d3b7a1de6b1ccb7f
Alice Lai Man Sze <alice_lai@SOONEST_HK>
2025-06-19 12:29:40 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,4 +2,17 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Framework7 主檔案（完整版）
+import Framework7 from 'framework7'
+
+// Vue plugin
+import Framework7Vue, { registerComponents } from '../node_modules/framework7-vue/framework7-vue-bundle'
+
+// CSS（直接引入 bundle）
+import '../node_modules/framework7/framework7-bundle.css'
+
+Framework7.use(Framework7Vue)
+
+const app = createApp(App)
+registerComponents(app)
+app.mount('#app')
